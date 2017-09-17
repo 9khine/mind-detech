8028a73279cda67ce2ed241e836d43849d324304
f0v8 <f0v8>
2017-09-17 13:38:11 -0700

    update

---
+++ front-end/display-eeg.js
@@ -0,0 +1,15 @@
+function myFunction(val) {
+	var display = document.getElementById('display');
+	var img-h = document.getElementById('eeg-h');
+	var img-ab = document.getElementById('eeg-ab');
+	if (val == 1) {
+		img-h.style.display = '';
+	} else {
+		img-ab.style.display = '';
+	}; 
+
+} 
+
+			
+			
+			
